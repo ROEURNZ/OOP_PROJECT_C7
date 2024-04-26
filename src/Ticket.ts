7a7498dcf94def30df07873e4825db0e91a80d81
ROEURNZ <roeurnkaki@gmail.com>
2024-04-26 14:50:41 +0700

    Create Ticket Entity for a Flight

---
+++ src/Ticket.ts
@@ -0,0 +1,47 @@
+import { Flight } from './Flight';
+import { Aircraft } from './Aircraft';
+import { Meal } from './Meal';
+import { Employee } from './Employee';
+import { Schedule } from './Schedule';
+
+export class Ticket {
+    private fareBasis: string;
+    private classType: ClassType;
+    private seatNumber: string;
+    private mealType: MealType;
+    private passenger: Passenger;
+    private flightInstance: FlightInstance;
+
+    constructor(fareBasis: string, classType: ClassType, seatNumber: string, mealType: MealType, passenger: Passenger, flightInstance: FlightInstance) {
+        this.fareBasis = fareBasis;
+        this.classType = classType;
+        this.seatNumber = seatNumber;
+        this.mealType = mealType;
+        this.passenger = passenger;
+        this.flightInstance = flightInstance;
+    }
+
+    getFareBasis(): string {
+        return this.fareBasis;
+    }
+
+    getClassType(): ClassType {
+        return this.classType;
+    }
+
+    getSeatNumber(): string {
+        return this.seatNumber;
+    }
+
+    getMealType(): MealType {
+        return this.mealType;
+    }
+
+    getPassenger(): Passenger {
+        return this.passenger;
+    }
+
+    getFlightInstance(): FlightInstance {
+        return this.flightInstance;
+    }
+}
