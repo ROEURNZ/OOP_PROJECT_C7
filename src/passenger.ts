dff5a0db7f7faa8de64ab56d7107506edab4d695
CHHOEURN.VOTEY <CHHOEURN.VOTEY@PNC.LAN>
2024-04-26 14:01:16 +0700

    create passenger entity

---
+++ src/passenger.ts
@@ -0,0 +1,24 @@
+class Passenger extends Person {
+    private specialMeal: MealType;
+    private frequentFlyerMembership: string;
+    private bookingReferenceNumber: string;
+
+    constructor(id: string, name: string, email: string, phone: string, specialMeal: MealType, frequentFlyerMembership: string, bookingReferenceNumber: string) {
+        super(id, name, email, phone);
+        this.specialMeal = specialMeal;
+        this.frequentFlyerMembership = frequentFlyerMembership;
+        this.bookingReferenceNumber = bookingReferenceNumber;
+    }
+
+    getSpecialMeal(): MealType {
+        return this.specialMeal;
+    }
+
+    getFrequentFlyerMembership(): string {
+        return this.frequentFlyerMembership;
+    }
+
+    getBookingReferenceNumber(): string {
+        return this.bookingReferenceNumber;
+    }
+}
