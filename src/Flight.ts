5b537c28e852560b61f8de2852f82b3461006ceb
ROEURNZ <roeurnkaki@gmail.com>
2024-04-26 13:53:06 +0700

    Create a Flight Entity for Airport

---
+++ src/Flight.ts
@@ -0,0 +1,27 @@
+export class Flight {
+    private flightNumber: string;
+    private flightInstances: FlightInstance[];
+    private airline: Airline;
+
+    constructor(flightNumber: string, airline: Airline) {
+        this.flightNumber = flightNumber;
+        this.flightInstances = [];
+        this.airline = airline;
+    }
+
+    addFlightInstance(instance: FlightInstance) {
+        this.flightInstances.push(instance);
+    }
+
+    getFlightNumber(): string {
+        return this.flightNumber;
+    }
+
+    getFlightInstances(): FlightInstance[] {
+        return this.flightInstances;
+    }
+
+    getAirline(): Airline {
+        return this.airline;
+    }
+}
--- src/Main.ts
+++
@@ -1,5 +0,0 @@
-import { Passenger } from "./Passenger";
-
-let Narith = new Passenger(101, "Narith Boy S'mos!!", "086 936 150", "Bunnarith@gnail.com");
-
-console.log(Narith);
