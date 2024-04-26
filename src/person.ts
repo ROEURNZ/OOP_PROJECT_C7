bd64957261f1e1c5e553226de04f2dab3d4b8db0
CHHOEURN.VOTEY <CHHOEURN.VOTEY@PNC.LAN>
2024-04-26 13:53:58 +0700

    [imp] create person entity

--- src/person.ts
+++ src/person.ts
@@ -1,22 +1,28 @@
-export class Person {
+abstract class Person {
     private id: string;
     private name: string;
     private email: string;
     private phone: string;
 
-    constructor(){
-
+    constructor(id: string, name: string, email: string, phone: string) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
     }
 
-    getId(): string {
+    getID(): string {
         return this.id;
     }
+
     getName(): string {
         return this.name;
     }
+
     getEmail(): string {
         return this.email;
     }
+
     getPhone(): string {
         return this.phone;
     }
