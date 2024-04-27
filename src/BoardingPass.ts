c93b71733dd1648e4c86d446d518bf0dac9a5a0a
CHHOEURN.VOTEY <CHHOEURN.VOTEY@PNC.LAN>
2024-04-27 10:13:36 +0700

    create boardingPass

---
+++ src/BoardingPass.ts
@@ -0,0 +1,20 @@
+import { FlightInstance } from "./FlightInstance";
+import { Passenger } from "./Passenger";
+
+export class BoardingPass {
+    private passenger: Passenger;
+    private flightInstance: FlightInstance;
+
+    constructor(passenger: Passenger, flightInstance: FlightInstance) {
+        this.passenger = passenger;
+        this.flightInstance = flightInstance;
+    }
+
+    getPassenger(): Passenger {
+        return this.passenger;
+    }
+
+    getFlightInstance(): FlightInstance {
+        return this.flightInstance;
+    }
+}
